72c1d617d27c260d0e42e455e2c20e133f98368c
abhijeet-t09 <tripathi.abhijeet0996@gmail.com>
2022-09-24 12:06:11 +0530

    Handle Custom Exceptions

--- src/test/java/com/bridgelabz/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -17,22 +17,59 @@ class MoodAnalyserTest {
     @Test
     public void shouldReturnSadMood(){
         MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Sad Mood");
-        String result =  moodAnalyser.analyseMood();
-        Assertions.assertSame("SAD", result);
-
+        try {
+            String result = moodAnalyser.analyseMood();
+            Assertions.assertSame("SAD", result);
+        }
+        catch(MoodException e){
+            Assertions.assertEquals(MoodException.ExceptionType.NULL,e.type);
+        }
     }
 
     @Test
     public void shouldReturnHappyMood(){
-        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Happy Mood");
-        String result =  moodAnalyser.analyseMood();
-        Assertions.assertSame("HAPPY", result);
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in any Mood");
+        try {
+            String result = moodAnalyser.analyseMood();
+            Assertions.assertSame("HAPPY", result);
+        }
+        catch(MoodException e){
+            Assertions.assertEquals(MoodException.ExceptionType.NULL, e.type);
+
+        }
+    }
+    @Test
+    public void ShouldHandleNullMessage() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        try {
+            String result = moodAnalyser.analyseMood();
+            Assertions.assertSame("HAPPY", result);
+        }
+        catch(MoodException e){
+            Assertions.assertEquals("Message is Null", e.getMessage());
+        }
+
     }
 
-    @Test
-    public void givenNullValueShouldHandleException(){
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String result = moodAnalyser.analyseMood();
-        Assertions.assertSame("HAPPY", result);
-    }
+//    @Test
+//    public void shouldReturnSadMood(){
+//        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Sad Mood");
+//        String result =  moodAnalyser.analyseMood();
+//        Assertions.assertSame("SAD", result);
+//
+//    }
+//
+//    @Test
+//    public void shouldReturnHappyMood(){
+//        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Happy Mood");
+//        String result =  moodAnalyser.analyseMood();
+//        Assertions.assertSame("HAPPY", result);
+//    }
+//
+//    @Test
+//    public void givenNullValueShouldHandleException(){
+//        MoodAnalyser moodAnalyser = new MoodAnalyser();
+//        String result = moodAnalyser.analyseMood();
+//        Assertions.assertSame("HAPPY", result);
+//    }
 }
